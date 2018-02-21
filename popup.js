43afc6fb0b0ce645d0184b7843c09460d5d2f331
Timothy Baker <tbake0155@gmail.com>
2018-02-21 10:21:18 -0500

    js updates

--- popup.js
+++ popup.js
@@ -1,7 +1,28 @@
 
+function getCurrentTabUrl(callback){
+
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, (tabs) => {
+        var tab = tabs[0];
+        var url = tab.url;
+        console.assert(typeof url == 'string', 'tab.url should be a string');
+        callback(url);
+    });
+}
 
 function fakeIt(){
+    var script ='
            var str = document.body.innerHTML;
            var res = str.replace(/news/gi, "fake news");
-           document.body.innerHTML = res;
+           document.body.innerHTML = res;'
+
+    chrome.tabs.executeScript({
+        code: script
+    });
 }       
+
+
