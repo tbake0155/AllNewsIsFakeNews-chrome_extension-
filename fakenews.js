c804efa5bf1d90d2b4c994bc21a54e62dc133a17
Timothy Baker <tbake0155@gmail.com>
2018-02-20 22:31:47 -0500

    updated fakenews.js

--- fakenews.js
+++ fakenews.js
@@ -1,10 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkButton = document.getElementById('Fake It');
-    checkButton.addEventListener('click', function() {
 
+
+function fakeIt(){
            var str = document.body.innerHTML;
            var res = str.replace(/news/gi, "fake news");
            document.body.innerHTML = res;
-
-    }, false);
- }, false);          
+}       
