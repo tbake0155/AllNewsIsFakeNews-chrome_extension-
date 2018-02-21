dc7f77a4a8b8272705100b81a46a9c53f0e34be3
Timothy Baker <tbake0155@gmail.com>
2018-02-20 21:47:38 -0500

    added json, js, and html files

---
+++ fakenews.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkButton = document.getElementById('Fake It');
+    checkPageButton.addEventListener('click', function() {
+
+        chrome.tabs.getSelected(null, function(tab) {
+           var str = document.innerHTML;
+           var res = str.replace(/news/gi, "fake news");
+           document.innerHTML = res;
+        });
+    }, false);
+ }, false);          
